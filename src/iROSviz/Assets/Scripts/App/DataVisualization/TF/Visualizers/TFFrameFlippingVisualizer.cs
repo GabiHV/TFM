67cdf4de9c45bd0e4e0b3d5fb209135e0647bf6f
Gabriel Hernandez Vallejo <ghernande438@alumno.uned.es>
2026-06-01 20:00:21 +0200

    Issue #48 - Applied flipping visualization.

--- src/iROSviz/Assets/Scripts/App/DataVisualization/TF/Visualizers/TFFrameFlippingVisualizer.cs
+++ src/iROSviz/Assets/Scripts/App/DataVisualization/TF/Visualizers/TFFrameFlippingVisualizer.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -7,9 +8,15 @@ public class TFFrameFlippingVisualizer : MonoBehaviour
 {
     public TFRoot.FrameNode frame;
     public GameObject TFFrameObj;
+    public GameObject TFFrameGhost;
 
     private float flipping;
     private static float flippingThreshold = 0;
+    private Coroutine flippingCoroutine;
+    private Quaternion lastRotation;
+
+    void Start() =>
+        DespawnGhost();
 
     void Update() =>
         PerformFlippingDetection();
@@ -28,17 +35,82 @@ public class TFFrameFlippingVisualizer : MonoBehaviour
     {
         List<Quaternion> rotations = frame.GetHistRotation();
         Quaternion currentRot = frame.Rotation;
-        Quaternion lastRot = rotations.Last();
+        lastRotation = rotations.Last();
 
-        flipping = Quaternion.Dot(currentRot, lastRot);
-    }
-
-    private void ApplyFlippingWarning()
-    {
-        // if(!IsFlipping()) return;
-        Debug.Log($"Frame: {frame.Name}. Flipping: {flipping}");
+        flipping = Quaternion.Dot(currentRot, lastRotation);
     }
 
     private bool IsFlipping() =>
         flipping < flippingThreshold;
+
+    private void ApplyFlippingWarning()
+    {
+        if(!IsFlipping()) StopFlipping();
+        if(IsFlipping()) TriggerFlipping();
+    }
+
+    private void SpawnGhost()
+    {
+        TFFrameGhost.SetActive(true);     
+        Quaternion lastRot = frame.GetHistRotation().Last();
+
+        TFFrameGhost.transform.rotation = lastRot;
+    }
+
+    private void DespawnGhost() =>
+        TFFrameGhost.SetActive(false);
+
+    private bool IsGhostSet() =>
+        TFFrameGhost != null;
+
+    private void TriggerFlipping()
+    {
+        if(!IsGhostSet()) return;
+        if(IsFlippingActive()) return;
+        SpawnGhost();
+        StartFlipping();
+    }
+
+    private void StartFlipping() =>
+        flippingCoroutine = StartCoroutine(FadeAndDestroyGhost(TFFrameGhost));
+
+    private void StopFlipping()
+    {
+        if(!IsFlippingActive()) return;
+        StopCoroutine(flippingCoroutine);
+        DespawnGhost();
+    }
+
+    private bool IsFlippingActive() =>
+        flippingCoroutine != null;
+
+    IEnumerator FadeAndDestroyGhost(GameObject ghost)
+    {
+        Renderer[] renderers = ghost.GetComponentsInChildren<Renderer>();
+        Debug.Log($"Renderers count: {renderers.Length}");
+
+        while (true)
+        {  
+            float t = 0f;
+            float duration = 1f;
+            while (t < duration)
+            {
+                float k = t / duration;
+
+                ghost.transform.rotation = 
+                    Quaternion.Slerp(frame.Rotation, lastRotation, k);
+                float alpha = Mathf.Lerp(1f, 0f, k);
+
+                foreach (var r in renderers)
+                {
+                    Color c = r.material.color;
+                    c.a = alpha;
+                    r.material.color = c;
+                }
+
+                t += Time.deltaTime;
+                yield return null;
+            }
+        }
+    }
 }
