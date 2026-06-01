4dc322496c39744ac5552882196ee9a880944e9d
Gabriel Hernandez Vallejo <ghernande438@alumno.uned.es>
2026-06-01 17:55:54 +0200

    Issue #46 - Applied visual jump marker

--- src/iROSviz/Assets/Scripts/App/DataVisualization/TF/Visualizers/TFFrameJumpVisualizer.cs
+++ src/iROSviz/Assets/Scripts/App/DataVisualization/TF/Visualizers/TFFrameJumpVisualizer.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -7,11 +8,16 @@ public class TFFrameJumpVisualizer : MonoBehaviour
 {
     public TFRoot.FrameNode frame;
     public GameObject TFFrameObj;
+    public GameObject JumpGO;
     public float distanceThreshold;
     public float rotationThreshold;
 
     private float deltaDistance;
     private float deltaRotation;
+    private Coroutine jitterCoroutine;
+
+    void Start() =>
+        JumpGO.SetActive(false);
 
     void Update() =>
         PerformJumpDetection();
@@ -25,7 +31,6 @@ public class TFFrameJumpVisualizer : MonoBehaviour
         JumpDetection();
         RotationJumpDetection();
         ApplyJumpWarning();
-        ApplyRotationJumpWarning();
     }
 
     private void JumpDetection()
@@ -54,11 +59,67 @@ public class TFFrameJumpVisualizer : MonoBehaviour
 
     private void ApplyJumpWarning()
     {
-        Debug.Log($"Frame: {frame.Name}. DeltaDistance: {deltaDistance}");
+        if(!IsJump() && !IsRotationJump()) 
+        {
+            StopJitterEffect();
+            return;
+        };
+        TriggerJump();
     }
 
-    private void ApplyRotationJumpWarning()
+
+    private void TriggerJump()
     {
-        Debug.Log($"Frame: {frame.Name}. DeltaRotatiopn: {deltaRotation}");
+        if(!IsJumpGOSet()) return;
+        if(IsJitterActive()) return;
+        StartJitterEfect();
     }
+
+    private bool IsJitterActive() =>
+        jitterCoroutine != null;
+    
+    private void StopJitterEffect()
+    {
+        if(!IsJitterActive()) return;
+        StopCoroutine(jitterCoroutine);
+
+        JumpGO.SetActive(false);
+        jitterCoroutine = null;
+    }
+
+    private void StartJitterEfect() =>
+        jitterCoroutine = StartCoroutine(JumpEffect(JumpGO.transform));
+
+    private IEnumerator JumpEffect(Transform jumpMarkerT)
+    {
+        jumpMarkerT.gameObject.SetActive(true);
+
+        Vector3 startScale = Vector3.one * 1f;
+        Vector3 endScale = Vector3.one * 2f;
+
+        Renderer rend = jumpMarkerT.GetComponent<Renderer>();
+
+        while (true)
+        {
+            float duration = 1f;
+            float t = 0;
+            while (t < duration)
+            {
+                float k = t / duration;
+
+                jumpMarkerT.localScale = Vector3.Lerp(startScale, endScale, k);
+
+                Color c = Color.Lerp(Color.yellow, Color.red, k);
+                c.a = 1f - k;
+
+                rend.material.color = c;
+
+                t += Time.deltaTime;
+                yield return null;
+            }
+        }
+    }
+
+    private bool IsJumpGOSet() =>
+        JumpGO != null;
 }
